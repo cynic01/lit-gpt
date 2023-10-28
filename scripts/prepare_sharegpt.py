5963dd65dfce8b9043a18086d021bd6d69449621
Edison <zx.chen@berkeley.edu>
2023-10-28 10:23:24 +0000

    sharegpt script

---
+++ scripts/prepare_sharegpt.py
@@ -0,0 +1,182 @@
+"""Implementation derived from prepare_csv.py and prepare_dolly.py"""
+import json
+import logging
+import sys
+from pathlib import Path
+from typing import Optional
+
+import requests
+import torch
+from torch.utils.data import random_split
+from tqdm import tqdm
+import pandas as pd
+
+# support running without installing as a package
+wd = Path(__file__).parent.parent.resolve()
+logger = logging.getLogger(__name__)
+sys.path.append(str(wd))
+
+from lit_gpt.tokenizer import Tokenizer
+
+COLUMNS = ("instruction", "input", "output")
+
+
+def prepare(
+    destination_path: Path = Path("data/sharegpt"),
+    checkpoint_dir: Path = Path("checkpoints/EleutherAI/pythia-1b-deduped"),
+    test_split_fraction: float = 0.1,
+    seed: int = 42,
+    mask_inputs: bool = True,
+    data_file_urls = ["https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/HTML_cleaned_raw_dataset/sg_90k_part1_html_cleaned.json",
+                      "https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/HTML_cleaned_raw_dataset/sg_90k_part2_html_cleaned.json"],
+    ignore_index: int = -1,
+    max_seq_length: Optional[int] = 256,
+) -> None:
+    """Prepare a CSV dataset for instruction tuning.
+
+    The output is a training and test dataset saved as `train.pt` and `test.pt`,
+    which stores the preprocessed and tokenized prompts and labels.
+    """
+    if max_seq_length is None:
+        with open(checkpoint_dir / "lit_config.json", "r") as file:
+            config = json.load(file)
+            max_seq_length = config["block_size"]
+
+    destination_path.mkdir(parents=True, exist_ok=True)
+    data_file_paths = [destination_path / "sg_90k_part1_html_cleaned.json", destination_path / "sg_90k_part2_html_cleaned.json"]
+    logger.info("Loading data file ...")
+    download_if_missing(data_file_paths[0], data_file_urls[0])
+    download_if_missing(data_file_paths[1], data_file_urls[1])
+
+    part1 = pd.read_json(data_file_paths[0], orient='records')
+    part1 = part1.apply(process_conversation, axis=1)
+    part1 = part1.dropna(how='all')
+
+    part2 = pd.read_json(data_file_paths[1], orient='records')
+    part2 = part2.apply(process_conversation, axis=1)
+    part2 = part2.dropna(how='all')
+
+    df = pd.concat([part1, part2], axis=0, ignore_index=True).astype(str).fillna("")
+    df = df.sample(n=50151, random_state=seed, axis=0)  # random sample to keep same size with oasst1 + dolly
+    print(df.head(10))
+    if not (df.columns.values == COLUMNS).all():
+        raise ValueError(f"CSV columns must be {COLUMNS}, found {df.columns.values}")
+    data = json.loads(df.to_json(orient="records", indent=4))
+
+    print("Loading tokenizer...")
+    tokenizer = Tokenizer(checkpoint_dir)
+
+    # Partition the dataset into train and test
+    train_set, test_set = random_split(
+        data, [1.0 - test_split_fraction, test_split_fraction], generator=torch.Generator().manual_seed(seed)
+    )
+    train_set, test_set = list(train_set), list(test_set)
+
+    print(f"train has {len(train_set):,} samples")
+    print(f"test has {len(test_set):,} samples")
+
+    print("Processing train split ...")
+    train_set = [
+        prepare_sample(
+            example=sample,
+            tokenizer=tokenizer,
+            max_length=max_seq_length,
+            mask_inputs=mask_inputs,
+            ignore_index=ignore_index,
+        )
+        for sample in tqdm(train_set)
+    ]
+    torch.save(train_set, destination_path / "train.pt")
+
+    print("Processing test split ...")
+    test_set = [
+        prepare_sample(
+            example=sample,
+            tokenizer=tokenizer,
+            max_length=max_seq_length,
+            mask_inputs=mask_inputs,
+            ignore_index=ignore_index,
+        )
+        for sample in tqdm(test_set)
+    ]
+    torch.save(test_set, destination_path / "test.pt")
+
+
+def download_if_missing(file_path: Path, file_url: str) -> None:
+    """Downloads the raw json data file and saves it in the given destination."""
+    if file_path.exists():
+        return
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(requests.get(file_url).text)
+
+
+def process_conversation(row):
+    out = {}
+    assert len(row['conversations']) > 0
+    for i in range(len(row['conversations'])):
+        if row['conversations'][i]['from'] in ('gpt', 'chatgpt'):
+            if i == 0 or row['conversations'][i-1]['from'] != 'human':
+                continue
+            out['instruction'] = row['conversations'][i-1]['value']
+            out['input'] = None
+            out['output'] = row['conversations'][i]['value']
+            break  # only record the first turn
+    return pd.Series(out) if 'instruction' in out else None
+
+
+def prepare_sample(example: dict, tokenizer: Tokenizer, max_length: int, mask_inputs: bool, ignore_index: int) -> dict:
+    """Processes a single sample.
+
+    Each sample in the dataset consists of:
+    - instruction: A string describing the task
+    - input: A string holding a special input value for the instruction.
+        This only applies to some samples, and in others this is empty.
+    - output: The response string
+
+    This function processes this data to produce a prompt text and a label for
+    supervised training. The prompt text is formed as a single message including both
+    the instruction and the input. The label/target is the same message but with the
+    response attached.
+
+    Finally, both the prompt and the label get tokenized. If desired, all tokens
+    in the label that correspond to the original input prompt get masked out (default).
+    """
+    full_prompt = generate_prompt(example)
+    full_prompt_and_response = full_prompt + example["output"]
+    encoded_full_prompt = tokenizer.encode(full_prompt, max_length=max_length)
+    encoded_full_prompt_and_response = tokenizer.encode(full_prompt_and_response, eos=True, max_length=max_length)
+
+    # The labels are the full prompt with response, but with the prompt masked out
+    labels = encoded_full_prompt_and_response.clone()
+    if mask_inputs:
+        labels[: len(encoded_full_prompt)] = ignore_index
+
+    return {
+        **example,
+        "input_ids": encoded_full_prompt_and_response,
+        "input_ids_no_response": encoded_full_prompt,
+        "labels": labels,
+    }
+
+
+def generate_prompt(example: dict) -> str:
+    """Generates a standardized message to prompt the model with an instruction, optional input and a
+    'response' field."""
+
+    if example["input"]:
+        return (
+            "Below is an instruction that describes a task, paired with an input that provides further context. "
+            "Write a response that appropriately completes the request.\n\n"
+            f"### Instruction:\n{example['instruction']}\n\n### Input:\n{example['input']}\n\n### Response:"
+        )
+    return (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        f"### Instruction:\n{example['instruction']}\n\n### Response:"
+    )
+
+
+if __name__ == "__main__":
+    from jsonargparse import CLI
+
+    CLI(prepare, as_positional=False)
